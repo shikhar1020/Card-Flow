2f2fb22644e62a08270d521afb10fb212e44f559
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-06-01 16:45:51 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Credit Card</h1>
+    </div>
+  );
+}
+
+export default App;
