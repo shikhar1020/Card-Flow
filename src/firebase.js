fdbbb504dd38e848bea5c1b5ca22c1260b55e688
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-06-04 09:53:28 +0530

    firebase import

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB68QcDYzkaccD4gkj7TqA5gALrtqnhOHw",
+  authDomain: "card-flow.firebaseapp.com",
+  projectId: "card-flow",
+  storageBucket: "card-flow.appspot.com",
+  messagingSenderId: "1027987147649",
+  appId: "1:1027987147649:web:6e6cdbcf7df756e3d6b680",
+  measurementId: "G-LVRH1BVDC3",
+};
