7268d31755c672406d8f695a2b3a9e46de880e98
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-06-05 16:12:58 +0530

    dropdown ui update

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Cards from "react-credit-cards";
+import { Select } from "antd";
 // import CreditCardInput from "react-credit-card-input";
 // import "react-credit-cards/es/styles-compiled.css";
 import "react-credit-cards/lib/styles.scss";
+import "antd/dist/antd.css";
 
 import "./Card.scss";
 
@@ -11,15 +13,17 @@ export default function Card() {
   const [expiry, setExpiry] = useState("");
   const [focus, setFoucs] = useState("");
   const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
+  const [number, setNumber] = useState(""); //#### #### #### ####
   // const [prev, setPrev] = useState(0);
 
   const [month, setMonth] = useState("MM");
   const [year, setYear] = useState("YY");
 
   //   Card Porops
-  const namePlaceholder = { name: "Card Holder" };
-  const expiryPlaceholder = { valid: "Expires" };
+  const namePlaceholder = { name: "AD SOYAD" };
+  const expiryPlaceholder = { valid: "Expires", color: "gray" };
+
+  const { Option } = Select;
 
   //   const handleInputFocus = (e) => {
   //     this.setState({ focus: e.target.name });
@@ -34,6 +38,14 @@ export default function Card() {
     setYear(e.target.value);
     // console.log("The selected year is:", year);
   };
+
+  function selectAntdMonth(value) {
+    setMonth(value);
+  }
+
+  function selectAntdYear(value) {
+    setYear(value);
+  }
 
   useEffect(() => {
     // setMonth()
@@ -113,49 +125,118 @@ export default function Card() {
           <div className="lastRow">
             <div className="lastRowExpiry">
               <label for="expiry">Expiration Date</label>
-              <select
-                onChange={(e) => selectMonth(e)}
-                value={month}
-                name="Month"
-                id="month"
-              >
-                {/* <option selected disabled>
+              <div className="expiryDateDropdownd">
+                <div className="dropdown" style={{ width: "200px" }}>
+                  <select
+                    onChange={(e) => selectMonth(e)}
+                    value={month}
+                    name="Month"
+                    id="month"
+                    className="select"
+                  >
+                    {/* <option selected disabled>
                   Month
                 </option> */}
-                <option value="MM" disabled selected label="✓ Month" />
-                <option value="01">January</option>
-                <option value="02">February</option>
-                <option value="03">March</option>
-                <option value="04">April</option>
-                <option value="05">May</option>
-                <option value="06">June</option>
-                <option value="07">July</option>
-                <option value="08">August</option>
-                <option value="09">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-              </select>
-              <select
-                onChange={(e) => selectYear(e)}
-                value={year}
-                name="Year"
-                id="year"
-              >
-                {/* <optgroup selected="selected" label="✓ Year"> */}
-                <option value="YY" disabled selected label="✓ Year" />
-                <option value="21">2021</option>
-                <option value="22">2022</option>
-                <option value="23">2023</option>
-                <option value="24">2024</option>
-                <option value="25">2025</option>
-                <option value="26">2026</option>
-                <option value="27">2027</option>
-                <option value="28">2028</option>
-                <option value="29">2029</option>
-                <option value="30">2030</option>
-                {/* </optgroup> */}
-              </select>
+                    {/* <div className="complete"> */}
+                    <option
+                      className="option month"
+                      value="MM"
+                      disabled
+                      selected
+                      label="✓ Month"
+                    />
+                    <option className="option" value="01">
+                      01
+                    </option>
+                    <option className="option" value="02">
+                      02
+                    </option>
+                    <option className="option" value="03">
+                      03
+                    </option>
+                    <option className="option" value="04">
+                      04
+                    </option>
+                    <option className="option" value="05">
+                      05
+                    </option>
+                    <option className="option" value="06">
+                      06
+                    </option>
+                    <option className="option" value="07">
+                      07
+                    </option>
+                    <option className="option" value="08">
+                      08
+                    </option>
+                    <option className="option" value="09">
+                      09
+                    </option>
+                    <option className="option" value="10">
+                      10
+                    </option>
+                    <option className="option" value="11">
+                      11
+                    </option>
+                    <option className="option" value="12">
+                      12
+                    </option>
+                    {/* </div> */}
+                  </select>
+                </div>
+                <div className="dropdown" style={{ width: "200px" }}>
+                  <select
+                    onChange={(e) => selectYear(e)}
+                    value={year}
+                    name="Year"
+                    id="year"
+                    className="select"
+                  >
+                    {/* <option selected disabled>
+                  Month
+                </option> */}
+                    {/* <div className="complete"> */}
+                    <option
+                      className="option month"
+                      value="YY"
+                      disabled
+                      selected
+                      label="✓ Year"
+                    />
+                    <option className="option" value="21">
+                      2021
+                    </option>
+                    <option className="option" value="22">
+                      2022
+                    </option>
+                    <option className="option" value="23">
+                      2023
+                    </option>
+                    <option className="option" value="24">
+                      2024
+                    </option>
+                    <option className="option" value="25">
+                      2025
+                    </option>
+                    <option className="option" value="26">
+                      2026
+                    </option>
+                    <option className="option" value="27">
+                      2027
+                    </option>
+                    <option className="option" value="28">
+                      2028
+                    </option>
+                    <option className="option" value="29">
+                      2029
+                    </option>
+                    <option className="option" value="2030">
+                      2030
+                    </option>
+                    {/* </div> */}
+                  </select>
+                </div>
+              </div>
               {/* <input
                 id="expiry"
                 type="text"
@@ -181,6 +262,7 @@ export default function Card() {
               />
             </div>
           </div>
+
           <input type="submit" value="Submit" />
         </form>
       </div>
